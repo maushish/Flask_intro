8e3a9e7ecc254c049cd6839a3b03dd5651f5d52a
maushish <chamarnumber102@gmail.com>
2023-10-18 20:38:10 +0530

    A basic todo finished

--- app.py
+++ app.py
@@ -35,9 +35,28 @@ def index():
         tasks=Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
-        
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete =Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "there was a problem deleting that task"
 
-
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    task=Todo.query.get_or_404(id)
+    if request.method=='POST':
+        task.content= request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'there was an issue in updating your task'
+    else:
+        return render_template('update.html', task=task)
 
 
 
